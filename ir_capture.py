7aef8a3a667a6ef931ef10c32846e83bb9f6af64
BillSavart <energyy0512@gmail.com>
2018-11-03 14:40:15 +0800

    Almost Finished

--- ir_capture.py
+++ ir_capture.py
@@ -4,12 +4,25 @@ import subprocess
 import cv2
 import numpy as np
 
-move = os.chdir("/home/pi/Pictures")
-argv = sys.argv[1]
-command = "raspistill -o " + argv
+file_name = sys.argv[1]
+
+if len(sys.argv) == 4:
+    file_path = sys.argv[3]
+    move = os.chdir(file_path)
+else:
+    move = os.chdir("/home/pi/Pictures")
+
+if len(sys.argv) == 3:
+    int_argv = int(sys.argv[2])
+    int_delay_time = (int_argv * 1000)
+    delay_time = str(int_delay_time)
+    command = "raspistill -o " + file_name + " -t " + delay_time
+else:
+    command = "raspistill -o " + file_name
+
 take_photo = subprocess.call(command, shell = True)
 
-image = cv2.imread("image.jpg", -1)
+image = cv2.imread(file_name, -1)
 
 des_image = cv2.namedWindow("Photo")
 cv2.imshow("Photo", image)
